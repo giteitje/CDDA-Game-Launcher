2b8fc26e0aeaac21e953291bdb3fe63badc96ac6
GitEitje <gitclient@redmawashi.com>
2016-08-07 00:57:37 +0200

    Move ExceptionWindow to a new file

---
+++ cddagl/ui/ExceptionWindow.py
@@ -0,0 +1,80 @@
+import gettext
+import html
+import traceback
+
+_ = gettext.gettext
+
+from io import StringIO
+from urllib.parse import urlencode
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QTextBrowser, \
+    QPushButton
+
+from cddagl.__version__ import version
+from cddagl.constants import NEW_ISSUE_URL
+
+
+class ExceptionWindow(QWidget):
+    def __init__(self, extype, value, tb):
+        super(ExceptionWindow, self).__init__()
+
+        layout = QGridLayout()
+
+        information_label = QLabel()
+        information_label.setText(_('The CDDA Game Launcher just crashed. An '
+            'unhandled exception was raised. Here are the details.'))
+        layout.addWidget(information_label, 0, 0)
+        self.information_label = information_label
+
+        tb_io = StringIO()
+        traceback.print_tb(tb, file=tb_io)
+        traceback_content = html.escape(tb_io.getvalue()).replace('\n', '<br>')
+
+        text_content = QTextBrowser()
+        text_content.setReadOnly(True)
+        text_content.setOpenExternalLinks(True)
+        text_content.setHtml(_('''
+<p>CDDA Game Launcher version: {version}</p>
+<p>Type: {extype}</p>
+<p>Value: {value}</p>
+<p>Traceback:</p>
+<code>{traceback}</code>
+''').format(version=html.escape(version), extype=html.escape(str(extype)),
+    value=html.escape(str(value)),
+    traceback=traceback_content))
+
+        layout.addWidget(text_content, 1, 0)
+        self.text_content = text_content
+
+        report_url = NEW_ISSUE_URL + '?' + urlencode({
+            'title': _('Unhandled exception: [Enter a title]'),
+            'body': _('''* Description: [Enter what you did and what happened]
+* Version: {version}
+* Type: `{extype}`
+* Value: {value}
+* Traceback:
+```
+{traceback}
+```
+''').format(version=version, extype=str(extype), value=str(value),
+    traceback=tb_io.getvalue())
+        })
+
+        report_label = QLabel()
+        report_label.setOpenExternalLinks(True)
+        report_label.setText(_('Please help us make a better launcher '
+            '<a href="{url}">by reporting this issue on GitHub</a>.').format(
+                url=html.escape(report_url)))
+        layout.addWidget(report_label, 2, 0)
+        self.report_label = report_label
+
+        exit_button = QPushButton()
+        exit_button.setText(_('Exit'))
+        exit_button.clicked.connect(self.close)
+        layout.addWidget(exit_button, 3, 0, Qt.AlignRight)
+        self.exit_button = exit_button
+
+        self.setLayout(layout)
+        self.setWindowTitle(_('Something went wrong'))
+        self.setMinimumSize(350, 0)
--- cddagl/ui/core_ui.py
+++ cddagl/ui/core_ui.py
@@ -8,7 +8,6 @@ import re
 import shutil
 import subprocess
 import sys
-import traceback
 import zipfile
 
 import cddagl.globals as globals
@@ -17,6 +16,7 @@ from cddagl.constants import READ_BUFFER_SIZE, MAX_GAME_DIRECTORIES, \
 from cddagl.ui.AboutDialog import AboutDialog
 from cddagl.ui.BackupsTab import BackupsTab
 from cddagl.ui.BrowserDownloadDialog import BrowserDownloadDialog
+from cddagl.ui.ExceptionWindow import ExceptionWindow
 from cddagl.ui.LauncherUpdateDialog import LauncherUpdateDialog
 from cddagl.ui.ModsTab import ModsTab
 from cddagl.ui.UpdateGroupBox import UpdateGroupBox
@@ -35,7 +35,7 @@ ngettext = gettext.ngettext
 
 from babel.core import Locale
 
-from io import BytesIO, StringIO
+from io import BytesIO
 from collections import deque
 
 import html5lib
@@ -502,6 +502,7 @@ class SettingsTab(QWidget):
     def enable_tab(self):
         self.launcher_settings_group_box.enable_controls()
         self.update_settings_group_box.enable_controls()
+
 
 class GameDirGroupBox(QGroupBox):
     def __init__(self):
@@ -2989,75 +2990,7 @@ class CataWindow(QWidget):
         rect = QRect(x, y, self.fontwidth, self.fontheight)
 
         painter.fillRect(rect, Qt.green)
-            
 
-# Recursively copy an entire directory tree while showing progress in a
-# status bar.
-
-
-class ExceptionWindow(QWidget):
-    def __init__(self, extype, value, tb):
-        super(ExceptionWindow, self).__init__()
-
-        layout = QGridLayout()
-
-        information_label = QLabel()
-        information_label.setText(_('The CDDA Game Launcher just crashed. An '
-            'unhandled exception was raised. Here are the details.'))
-        layout.addWidget(information_label, 0, 0)
-        self.information_label = information_label
-
-        tb_io = StringIO()
-        traceback.print_tb(tb, file=tb_io)
-        traceback_content = html.escape(tb_io.getvalue()).replace('\n', '<br>')
-
-        text_content = QTextBrowser()
-        text_content.setReadOnly(True)
-        text_content.setOpenExternalLinks(True)
-        text_content.setHtml(_('''
-<p>CDDA Game Launcher version: {version}</p>
-<p>Type: {extype}</p>
-<p>Value: {value}</p>
-<p>Traceback:</p>
-<code>{traceback}</code>
-''').format(version=html.escape(version), extype=html.escape(str(extype)),
-    value=html.escape(str(value)),
-    traceback=traceback_content))
-
-        layout.addWidget(text_content, 1, 0)
-        self.text_content = text_content
-
-        report_url = NEW_ISSUE_URL + '?' + urlencode({
-            'title': _('Unhandled exception: [Enter a title]'),
-            'body': _('''* Description: [Enter what you did and what happened]
-* Version: {version}
-* Type: `{extype}`
-* Value: {value}
-* Traceback:
-```
-{traceback}
-```
-''').format(version=version, extype=str(extype), value=str(value),
-    traceback=tb_io.getvalue())
-        })
-
-        report_label = QLabel()
-        report_label.setOpenExternalLinks(True)
-        report_label.setText(_('Please help us make a better launcher '
-            '<a href="{url}">by reporting this issue on GitHub</a>.').format(
-                url=html.escape(report_url)))
-        layout.addWidget(report_label, 2, 0)
-        self.report_label = report_label
-
-        exit_button = QPushButton()
-        exit_button.setText(_('Exit'))
-        exit_button.clicked.connect(self.close)
-        layout.addWidget(exit_button, 3, 0, Qt.AlignRight)
-        self.exit_button = exit_button
-
-        self.setLayout(layout)
-        self.setWindowTitle(_('Something went wrong'))
-        self.setMinimumSize(350, 0)
 
 def init_gettext(locale):
     locale_dir = os.path.join(globals.basedir, 'cddagl', 'locale')
